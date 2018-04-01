f721be0b15bd3483f5c4e044413836f75b6415ac
e.sevastyanov <sevastyanov.eugene@gmail.com>
2018-04-01 18:27:10 +0300

    Common Child

--- src/scala/training/hackerrank/Common_Child.scala
+++ src/scala/training/hackerrank/Common_Child.scala
@@ -14,27 +14,22 @@ object Common_Child extends App
 
     def maxChildLength(s1: String, s2: String): Int = {
       val (sMinL, sMaxL) = if (s1.length < s2.length) (s1, s2) else (s2, s1)
-      val charsMap = mutable.Map[Char, mutable.ArrayBuffer[Int]]()
-      (0 until sMaxL.length).foreach { i =>
-        val idxs = charsMap.getOrElseUpdate(sMaxL(i), mutable.ArrayBuffer())
-        idxs += i
-      }
+      val charsMap = Array.fill('Z' - 'A' + 1)(mutable.ArrayBuffer[Int]())
+      (0 until sMaxL.length).foreach { i => charsMap('Z' - sMaxL(i)) += i }
 
       def _count(i: Int, n: Int): Int = {
-        if (i >= sMinL.length) 0
-        else {
+        if (i >= sMinL.length) {
+          0
+        } else {
           val c = sMinL(i)
-          charsMap.get(c).map { idxs =>
-            val ns = idxs.dropWhile(_ <= n)
-            if (ns.nonEmpty) math.max(1 + _count(i + 1, ns.head), _count(i + 1, n))
-            else _count(i + 1, n)
-          }.getOrElse {
-            _count(i + 1, n)
-          }
+          charsMap('Z' - c)
+            .find(_ > n)
+            .map(nn => math.max(1 + _count(i + 1, nn), _count(i + 1, n)))
+            .getOrElse(_count(i + 1, n))
         }
       }
 
-      _count(0, 0)
+      _count(0, -1)
     }
 
     def main(args: Array[String]): Unit = {
