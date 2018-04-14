e9af0af8e4bd7bce3417df5c0b8511c2319f77b6
e.sevastyanov <sevastyanov.eugene@gmail.com>
2018-04-14 23:20:38 +0300

    Reverse Shuffle Merge

--- src/scala/training/hackerrank/Reverse_Shuffle_Merge.scala
+++ src/scala/training/hackerrank/Reverse_Shuffle_Merge.scala
@@ -9,50 +9,46 @@ object Reverse_Shuffle_Merge extends App
 
   object Solution
   {
+
     import scala.collection.mutable
 
     def findExtremePoint(s: String, m: Map[Char, Int], start: Int, end: Int): Int = {
       val mm = mutable.Map().withDefaultValue(0) ++ m
-      var i = start
-      while (i < end) {
-        val l = s(i)
-        if (mm(l) <= 1) mm -= l else mm(l) -= 1
-        if (mm.isEmpty) return i
-        i += 1
-      }
-      ???
-    }
-
-    def findMin(s: String, m: Map[Char, Int], start: Int, end: Int): Int = {
-      var minC = Char.MaxValue
-      var minI = -1
       (start to end).foreach { i =>
         val c = s(i)
-        if (m.contains(c)) {
-          if (minC == c) minI = i
-          else if (minC >= c) {
-            minC = c
-            minI = i
-          }
-        }
+        if (mm(c) <= 1) mm -= c else mm(c) -= 1
+        if (mm.isEmpty) return i
       }
-      minI
+      end
     }
 
-    def reverseShuffleMerge(s: String): String =  {
-      var letters = mutable.Map[Char, Int]().withDefaultValue(0)
+    def findMinPoint(s: String, m: Map[Char, Int], start: Int, end: Int): Int = {
+      var minc = Char.MaxValue
+      var mini = -1
+      (start to end).foreach { i =>
+        val c = s(i)
+        if (m.contains(c) && minc >= c) {
+          minc = c
+          mini = i
+        }
+      }
+      mini
+    }
+
+    def reverseShuffleMerge(s: String): String = {
+      val doubledLetters = mutable.Map[Char, Int]().withDefaultValue(0)
       s.indices.foreach { i =>
         val l = s(i)
-        letters(l) += 1
+        doubledLetters(l) += 1
       }
-      letters = letters.map {case (k, v) => (k, v/2)}
+      val letters = doubledLetters.map { case (k, v) => (k, v / 2) }
       val sb = new mutable.StringBuilder()
-      var ep = s.length
-      var minI = s.length - 1
+      var extremePoint = s.length
+      var minPoint = s.length
       while (letters.nonEmpty) {
-        ep = findExtremePoint(s, letters.toMap, 0, minI)
-        minI = findMin(s, letters.toMap, ep, minI)
-        val c = s(minI)
+        extremePoint = findExtremePoint(s, letters.toMap, 0, minPoint - 1)
+        minPoint = findMinPoint(s, letters.toMap, extremePoint, minPoint - 1)
+        val c = s(minPoint)
         sb += c
         if (letters(c) <= 1) letters -= c else letters(c) -= 1
       }
