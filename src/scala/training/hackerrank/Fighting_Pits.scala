ba5084dfad8441b7b183181b95b57c3f495f1697
e.sevastyanov <sevastyanov.eugene@gmail.com>
2018-04-17 23:08:30 +0300

    Fighting Pits

--- src/scala/training/hackerrank/Fighting_Pits.scala
+++ src/scala/training/hackerrank/Fighting_Pits.scala
@@ -11,64 +11,106 @@ object Fighting_Pits extends App
   object Solution
   {
 
-    import scala.collection.SortedMap
+    import scala.collection.mutable
 
-    class Team(val t: SortedMap[Int, Int])
+    private class Team
     {
-      private val iterator = t.keys.iterator
-      var strength: Int = iterator.next()
-      var health  : Int = t(strength)
+      val m: mutable.SortedMap[Int, Int] = mutable.SortedMap()
+
+      var strength: Int = 0
+      var count   : Int = 0
+
+      private var changed = true
+
+      private var fighters : Array[Int] = Array()
+      private var strengths: Array[Int] = Array()
+
+      def addFighter(s: Int): Unit = {
+        changed = true
+        val v = m.getOrElseUpdate(s, 0)
+        m(s) = v + 1
+        strength += s
+        count += 1
+      }
+
+      def getFighters: Array[Int] = {
+        refresh()
+        fighters
+      }
+
+      def getStrenghts: Array[Int] = {
+        refresh()
+        strengths
+      }
+
+      private def refresh(): Unit = {
+        if (changed) {
+          changed = false
+          fighters = new Array[Int](count)
+          strengths = new Array[Int](count)
+          var i = 0
+          m.foreach { case (k, v) =>
+            (1 to v).foreach { _ =>
+              fighters(i) = k
+              strengths(i) = if (i == 0) k else strengths(i - 1) + k
+              i += 1
+            }
+          }
+        }
+      }
+    }
+
+    private class TeamBattleHolder(val team: Team)
+    {
+      val fighters : Array[Int] = team.getFighters
+      var i        : Int        = team.getFighters.length - 1
+
+      def total: Int = if (i >= 0) team.getStrenghts(i) else 0
 
       def hit(damage: Int): Unit = {
-        if (health <= 0) {
-          strength = if (iterator.hasNext) iterator.next() else 0
-          health = t(strength)
-        }
-        if (damage > 0 && health > 0) {
-          val h = health
-          health -= damage
-          hit(damage - h)
-        }
+        i -= damage
       }
     }
 
-    def whoIsWinner(t1: SortedMap[Int, Int], t2: SortedMap[Int, Int]): Boolean = {
-      val tt1 = new Team(t1)
-      val tt2 = new Team(t2)
+    private def whoIsWinner(t1: Team, t2: Team): Team = {
+      val tt1 = new TeamBattleHolder(t1)
+      val tt2 = new TeamBattleHolder(t2)
+      if (tt2.i < 0) return t1
+      if (tt1.i < 0) return t2
       var oq = tt1
       var dq = tt2
-      while (oq.health > 0 && dq.health > 0) {
-        dq.hit(oq.strength)
-        val t = oq
-        oq = dq
-        dq = t
+      while (oq.total < dq.total && oq.i >= 0 && dq.i >= 0) {
+        dq.hit(oq.fighters(oq.i))
+        if (dq.i >= 0) {
+          val t = oq
+          oq = dq
+          dq = t
+        }
       }
-      tt1.health > 0
-    }
-
-    private def addFighter(team: SortedMap[Int, Int], s: Int) = {
-      team.updated(s, team.getOrElse(s, 0) + 1)
+      oq.team
     }
 
     def main(args: Array[String]): Unit = {
       val sc = new java.util.Scanner(System.in)
       val n, k, q = sc.nextInt()
-      val teams = Array.fill(k + 1)(SortedMap[Int, Int](0 -> 0)(Ordering.Int.reverse))
+      val teams = Array.fill(k + 1)(new Team())
       (1 to n).foreach { _ =>
         val s = sc.nextInt()
         val i = sc.nextInt()
-        teams(i) = addFighter(teams(i), s)
+        teams(i).addFighter(s)
       }
       (1 to q).foreach { _ =>
         sc.nextInt() match {
           case 1 =>
             val s = sc.nextInt()
             val i = sc.nextInt()
-            teams(i) = addFighter(teams(i), s)
+            teams(i).addFighter(s)
           case 2 =>
             val offender = sc.nextInt()
             val defender = sc.nextInt()
-            println(if (whoIsWinner(teams(offender), teams(defender))) offender else defender)
+            val tOff = teams(offender)
+            val tDef = teams(defender)
+            println(if (whoIsWinner(tOff, tDef) == tOff) offender else defender)
           case c => sys.error(s"Unknown command $c")
         }
       }
