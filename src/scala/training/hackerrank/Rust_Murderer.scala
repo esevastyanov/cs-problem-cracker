a01cf38243775d0543155df0382f1ff60cddebf5
e.sevastyanov <sevastyanov.eugene@gmail.com>
2019-02-09 20:58:01 +0700

    Rust & Murderer

--- src/scala/training/hackerrank/Rust_Murderer.scala
+++ src/scala/training/hackerrank/Rust_Murderer.scala
@@ -12,48 +12,103 @@ object Rust_Murderer extends App
 
     import scala.collection.mutable
 
-    def rustMurdered(n: Int, s: Int, g: mutable.Map[Int, mutable.Set[Int]]): Array[Int] = {
-      val r = Array.fill(n)(Int.MaxValue)
-
-      def _do(is: mutable.Set[Int]): Unit = {
-        if (is.isEmpty) return
-        val nextIs = mutable.Set[Int]()
-        is.foreach { i =>
-          g(i).foreach { j =>
-            if (r(i - 1) + 1 < r(j - 1)) {
-              r(j - 1) = r(i - 1) + 1
-              nextIs += j
-            }
+    def rustMurdered(n: Int, s: Int, g: Array[mutable.BitSet]): Array[Int] = {
+      val r = Array.fill(n)(1)
+      r(s) = 0
+      var currDst = 2
+      val notVisited = g(s)
+      val newlyVisited = mutable.Set[Int]()
+      while (notVisited.nonEmpty) {
+        notVisited.foreach { i =>
+          val diff = notVisited ++ g(i)
+          if (diff.size < n) {
+            r(i) = currDst
+            newlyVisited += i
           }
         }
-        _do(nextIs)
+        notVisited --= newlyVisited
+        newlyVisited.clear()
+        currDst += 1
       }
-
-      r(s - 1) = 0
-      _do(mutable.Set(s))
       r
     }
 
     def main(args: Array[String]): Unit = {
-      val sc = new java.util.Scanner(System.in)
+      val sc = new FastReader
       (1 to sc.nextInt()).foreach { _ =>
         val n = sc.nextInt()
-
-        def newMutableSet(): mutable.Set[Int] = mutable.Set(1 to n: _*)
-
-        val g = mutable.Map[Int, mutable.Set[Int]]((1 to n).map(_ -> newMutableSet()): _*)
+        val g = Array.fill(n)(mutable.BitSet())
         (1 to sc.nextInt()).foreach { _ =>
-          val x = sc.nextInt()
-          val y = sc.nextInt()
-          g(x).remove(x)
-          g(x).remove(y)
-          g(y).remove(x)
-          g(y).remove(y)
+          val x = sc.nextInt() - 1
+          val y = sc.nextInt() - 1
+          g(x) += y
+          g(y) += x
         }
-        rustMurdered(n, sc.nextInt(), g).foreach(i => if (i != 0) print(i + " "))
+        rustMurdered(n, sc.nextInt() - 1, g).foreach(i => if (i != 0) print(i + " "))
         println()
       }
     }
+
+    class FastReader()
+    {
+
+      import java.io.DataInputStream
+
+      private val BUFFER_SIZE   = 1 << 16
+      private val din           = new DataInputStream(System.in)
+      private val buffer        = new Array[Byte](BUFFER_SIZE)
+      private var bufferPointer = 0
+      private var bytesRead     = 0
+
+
+      def nextInt(): Int = {
+        var ret = 0
+        var c = read
+        while (c <= ' ') {
+          c = read
+        }
+        val neg = c == '-'
+        if (neg) c = read
+        do {
+          ret = ret * 10 + c - '0'
+          c = read
+        } while (c >= '0' && c <= '9')
+        val res = if (neg) -ret else ret
+        res
+      }
+
+      def next(): String = {
+        val sb = new StringBuilder()
+        var c = read
+        while (c != -1 && (c == '\n' || c == ' ' || c == '\t')) {
+          c = read
+        }
+        while (c != -1 && c != '\n' && c != ' ' && c != '\t') {
+          sb.append(c.toChar)
+          c = read
+        }
+        val res = sb.mkString
+        res
+      }
+
+      private def fillBuffer(): Unit = {
+        bufferPointer = 0
+        bytesRead = din.read(buffer, bufferPointer, BUFFER_SIZE)
+        if (bytesRead == -1) buffer(0) = -1
+      }
+
+      private def read = {
+        if (bufferPointer == bytesRead) fillBuffer()
+        bufferPointer += 1
+        buffer(bufferPointer - 1)
+      }
+
+      def close(): Unit = {
+        if (din == null) return
+        din.close()
+      }
+    }
+
   }
 
 }
