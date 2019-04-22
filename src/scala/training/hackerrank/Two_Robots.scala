a23a1841a8df4ce03d04a60b86730a32892c75f6
e.sevastyanov <sevastyanov.eugene@gmail.com>
2019-04-23 00:21:16 +0300

    Two Robots

--- src/scala/training/hackerrank/Two_Robots.scala
+++ src/scala/training/hackerrank/Two_Robots.scala
@@ -9,85 +9,42 @@ object Two_Robots extends App
 
   object Solution
   {
+    import scala.collection.mutable
 
     def minTotalDistance(n: Int, qs: Array[(Int, Int)]): Int = {
-      val state: Array[Array[States]] = Array.fill(n)(Array.fill(n)(null))
-      for (i <- 0 until n) {
-        for (j <- i until n) {
-          val (qa, qb) = qs(j)
-          if (i == 0 && j == 0) {
-            state(i)(j) = States(List(State(qb, 0, math.abs(qb - qa))))
-          } else if (i == 0) {
-            state(i)(j) = state(i)(j-1).execute1(qs(j))
-          } else if (i == j) {
-            state(i)(j) = state(i-1)(j-1).execute2(qs(j))
-          } else {
-//            state(i)(j) = state(i)(j-1).execute1(qs(j)).min(state(i-1)(j-1).execute2(qs(j)))
-            state(i)(j) = state(i)(j-1).execute1(qs(j)).and(state(i-1)(j-1).execute2(qs(j)))
+      var m1, m2 = mutable.Map[(Int, Int), State]()
+      val (qa, qb) = qs(0)
+      m1.put((0, qb), State(qb, 0, math.abs(qb - qa)))
+      (1 until n).foreach(i => {
+        val (qa, qb) = qs(i)
+        m1.foreach { case (p, s) =>
+          (0 to 1).foreach { j =>
+            val _s = s.execute(j)(qs(i))
+            if (!m2.contains(_s.p)) m2.put(_s.p, _s) else if (m2(_s.p).d > _s.d) m2(_s.p) = _s
           }
         }
+        m1 = m2
+        m2 = mutable.Map()
       }
-//      (0 until n).map(state(_)(n-1).ss.head).minBy(_.d).d
-      (0 until n).map(state(_)(n-1).ss.minBy(_.d).d).min
-    }
-
-    def minTotalDistanceRecursive(n: Int, qs: Array[(Int, Int)]): Int = {
-      var min = Int.MaxValue
-      def _do(i: Int, state: State, rr: String): Unit = {
-        if (i == n) {
-          min = math.min(min, state.d)
-        } else {
-          _do(i + 1, state.execute1(qs(i)), rr + "1")
-          _do(i + 1, state.execute2(qs(i)), rr + "2")
-        }
-      }
-      _do(0, State(), "")
-      min
-    }
-
-    case class States(ss: List[State]) {
-      import scala.collection.mutable
-
-      private def normalize(arr: List[State]): List[State] = {
-        val m = mutable.Map[(Int, Int), State]()
-        arr.foreach { s =>
-          val p = (math.min(s.p1, s.p2), math.max(s.p1, s.p2))
-          if (!m.contains(p)) {
-            m.put(p, s)
-          } else {
-            if (m(p).d < s.d) m(p) = s
-          }
-        }
-        m.values.toList
-      }
-
-      def execute1(q: (Int, Int)): States = States(normalize(ss.map(_.execute1(q))))
-
-      def execute2(q: (Int, Int)): States = States(normalize(ss.map(_.execute2(q))))
-
-      def min(that: States): States = {
-        val d = math.min(this.ss.minBy(_.d).d, that.ss.minBy(_.d).d)
-        States(normalize(this.ss ++ that.ss).filter(_.d == d))
-      }
-
-      def and(that: States): States = {
-        States(normalize(this.ss ++ that.ss))
-      }
+      )
+      m1.values.minBy(_.d).d
     }
 
     case class State(p1: Int = 0, p2: Int = 0, d: Int = 0)
     {
-      def execute1(q: (Int, Int)): State = {
+      val p: (Int, Int) = (p1, p2)
+      val arr = Array(p1, p2)
+
+      def execute(i: Int)(q: (Int, Int)): State = {
         val (qa, qb) = q
-        this.copy(p1 = qb, d = (if (p1 == 0) 0 else math.abs(p1 - qa)) + math.abs(qa - qb) + d)
+        val (_p1, _p2) = (math.min(qb, arr((i + 1) % 2)), math.max(qb, arr((i + 1) % 2)))
+        val _d = (if (arr(i) == 0) 0 else math.abs(arr(i) - qa)) + math.abs(qa - qb) + d
+        State(p1 = _p1, p2 = _p2, d = _d)
       }
 
-      def execute2(q: (Int, Int)): State = {
-        val (qa, qb) = q
-        this.copy(p2 = qb, d = (if (p2 == 0) 0 else math.abs(p2 - qa)) + math.abs(qa - qb) + d)
-      }
+      def execute1(q: (Int, Int)): State = execute(0)(q)
 
-      def min(s: State): State = if (this.d <= s.d) this else s
+      def execute2(q: (Int, Int)): State = execute(1)(q)
     }
 
     def main(args: Array[String]): Unit = {
@@ -95,7 +52,6 @@ object Two_Robots extends App
       (1 to sc.nextInt()).foreach { _ =>
         val m, n = sc.nextInt()
         val qs = Array.fill(n)((sc.nextInt(), sc.nextInt()))
-//        println(minTotalDistanceRecursive(n, qs))
         println(minTotalDistance(n, qs))
       }
     }
@@ -110,12 +66,3 @@ object Two_Robots extends App
 // 40
 // 1 9 6  4 8  3 9  8 2  5 9  6 9  1 4
 // 34
-
-/*
-8 0 4
-8 9 10
-2 9 16
-2 9 24
-2 9 30
-4 9 34
-*/
