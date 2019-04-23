cdfe657dde6ef93f9221b3b9b55cc2176e84c5cb
e.sevastyanov <sevastyanov.eugene@gmail.com>
2019-04-23 16:39:05 +0300

    Travel Around The World

--- src/scala/training/hackerrank/Travel_Around_The_World.scala
+++ src/scala/training/hackerrank/Travel_Around_The_World.scala
@@ -10,19 +10,32 @@ object Travel_Around_The_World extends App
   object Solution
   {
 
-    def travelAroundTheWorld(n: Int, c: Long, a: Array[Int], b: Array[Int]): Int = {
-      var total = n
-
-      def check(i: Int, f: Int): Unit = {
-        if (math.min(a(i), c) + f < b(i) && total > 0) {
-          total -= 1
-          check((i - 1 + n) % n, f + math.min(a(i), c).toInt - b(i))
+    def travelAroundTheWorld(n: Int, c: Long, a: Array[Long], b: Array[Long]): Int = {
+      val m = Array.fill(n)(Array.fill(n)(0L))
+      for (i <- 0 until n) {
+        for (j <- i + 1 until n + i + 1) {
+          val prev = (j + n - 1) % n
+          if (m(i)(prev) >= 0) {
+            m(i)(j % n) = math.min(m(i)(prev) + a(prev), c) - b(prev)
+          } else {
+            m(i)(j % n) = m(i)(prev)
+          }
         }
       }
+      (0 until n).count(i => m(i)(i) >= 0)
+    }
 
-      a.indices.foreach { i =>
-        if (b(i) > c) return 0
-        if (a(i) < b(i)) check(i, 0)
+    def _travelAroundTheWorld(n: Int, c: Long, a: Array[Long], b: Array[Long]): Int = {
+      var total = 0
+      for (i <- 0 until n) {
+        var tank = 0L
+        for (j <- i + 1 until n + i + 1) {
+          val prev = (j + n - 1) % n
+          if (tank >= 0) {
+            tank = math.min(tank + a(prev), c) - b(prev)
+          }
+        }
+        if (tank >= 0) total += 1
       }
       total
     }
@@ -31,9 +44,9 @@ object Travel_Around_The_World extends App
       val sc = new java.util.Scanner(System.in)
       val n = sc.nextInt()
       val c = sc.nextLong()
-      val a = Array.fill(n)(sc.nextInt())
-      val b = Array.fill(n)(sc.nextInt())
-      println(travelAroundTheWorld(n, c, a, b))
+      val a = Array.fill(n)(sc.nextLong())
+      val b = Array.fill(n)(sc.nextLong())
+      println(_travelAroundTheWorld(n, c, a, b))
     }
   }
 
