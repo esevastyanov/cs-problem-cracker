9dd84ecc96a955679123f8b650d249e9b2b5554e
e.sevastyanov <sevastyanov.eugene@gmail.com>
2019-06-15 14:00:09 +0300

    Full Counting Sort

--- src/scala/training/hackerrank/Full_Counting_Sort.scala
+++ src/scala/training/hackerrank/Full_Counting_Sort.scala
@@ -10,16 +10,19 @@ object Full_Counting_Sort extends App
   object Solution
   {
 
+    import scala.collection.mutable.ListBuffer
+
     def main(args: Array[String]): Unit = {
       val sc = new java.util.Scanner(System.in)
       val a = Array.fill(sc.nextInt())((sc.nextInt(), sc.next()))
-      println(
-        a.zipWithIndex
-          .map { case ((i, s), j) => if (j >= a.length / 2) (i, s) else (i, "-") }
-          .sortBy(_._1)
-          .map(_._2)
-          .mkString(" ")
-      )
+      val m = Array.fill(100)(ListBuffer[String]())
+      for (j <- a.indices) {
+        val (i, s) = if (j >= a.length / 2) a(j) else a(j).copy(_2 = "-")
+        m(i) += s
+      }
+      m.foreach { l =>
+        if (l.nonEmpty) print(l.mkString(" ") + " ")
+      }
     }
   }
 
