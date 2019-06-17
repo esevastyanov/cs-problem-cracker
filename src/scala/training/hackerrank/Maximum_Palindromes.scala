f12d78f7c4d59c9333b13a6026eadf12d7a76efc
e.sevastyanov <sevastyanov.eugene@gmail.com>
2019-06-17 12:54:31 +0300

    Maximum Palindromes

--- src/scala/training/hackerrank/Maximum_Palindromes.scala
+++ src/scala/training/hackerrank/Maximum_Palindromes.scala
@@ -10,26 +10,34 @@ object Maximum_Palindromes extends App
   object Solution
   {
 
-    val cache: Array[BigInt] = Array.fill(100001)(BigInt(0))
-    cache(0) = 1
-    cache(1) = 1
+    val cache: Array[Long] = Array.fill(100001)(0L)
+    cache(0) = 1L
+    cache(1) = 1L
 
-    def factorial(n: Int): BigInt = {
-      if (cache(n) != 0) cache(n)
-      val res = n * factorial(n - 1)
+    def factorial(n: Int, p: Int): Long = {
+      if (cache(n) != 0) return cache(n)
+      val res = factorial(n - 1, p) * n % p
       cache(n) = res
       res
     }
 
-    def countMaxPalindromes(s: String, i: Int, j: Int): Int = {
+    def exp(n: Long, e: Int, p: Int): Long = {
+      if (e == 0) return 1
+      if (e == 1) return n
+      if (e % 2 == 0) exp(n * n % p, e / 2, p)
+      else n * exp(n * n % p, (e - 1) / 2, p) % p
+    }
+
+    def countMaxPalindromes(s: String, i: Int, j: Int): Long = {
       val a = Array.fill('z' - 'a' + 1)(0)
       for (k <- i to j) {
         a(s(k) - 'a') += 1
       }
       val halves = a.map(_ / 2)
       val p = 1000000007
-      val res = factorial(halves.sum) / halves.map(factorial).product * math.max(1, a.count(_ % 2 == 1)) % p
-      res.toInt
+      val res =
+        ((factorial(halves.sum, p) * (halves.map(factorial(_, p)).map(exp(_, p-2, p)).reduce(_ * _ % p) % p) % p) * math.max(1, a.count(_ % 2 == 1))) % p
+      res
     }
 
     def main(args: Array[String]): Unit = {
